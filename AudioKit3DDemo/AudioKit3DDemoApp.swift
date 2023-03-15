7819314151207f005c0bffcb0685e6487efbcfb3
Stan Rosenbaum <ForeverTangent@gmail.com>
2023-03-15 19:54:24 -0400

    Initial Commit

---
+++ AudioKit3DDemo/AudioKit3DDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKit3DDemoApp.swift
+//  AudioKit3DDemo
+//
+//  Created by Stanley Rosenbaum on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioKit3DDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
