9fa481b3a776ccb8e264f92a90580f39ca471f6d
Stan Rosenbaum <ForeverTangent@gmail.com>
2023-03-16 08:17:40 -0400

    Fixed File Location.

--- AudioKit3DDemo/CookbookCommon/Reusable Components/Cookbook.swift
+++ AudioKit3DDemo/CookbookCommon/Reusable Components/Cookbook.swift
@@ -6,7 +6,8 @@ import SwiftUI
 // Helper functions
 class Cookbook {
     static var sourceBuffer: AVAudioPCMBuffer {
-        let url = Bundle.main.resourceURL?.appendingPathComponent("Samples/beat.aiff")
+		let url = Bundle.main.url(forResource: "beat", withExtension: "aiff")
+//        let url = Bundle.main.resourceURL?.appendingPathComponent("Samples/beat.aiff")
         let file = try! AVAudioFile(forReading: url!)
         return try! AVAudioPCMBuffer(file: file)!
     }
